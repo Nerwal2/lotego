9c6002a784da14319dd5b4baf4f00fe3f4f9c17a
Nerwal2 <master4567890@gmail.com>
2021-10-05 11:57:53 +0000

    indexv2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,56 @@
-
-   
+import React from "react";
 import ReactDOM from "react-dom";
 
 
-ReactDOM.render(
-    <h1>Hello World</h1>,
-    document.getElementById('root')
-  );
+
+class StatefulComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "World",
+      date: new Date(),
+    };
+  }
+
+  handleChange(event) {
+    const name = event.target.value;
+    this.setState({ name: name });
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  componentDidMount() {
+    console.log("Mounted!");
+    setInterval(() => {
+      console.log("tick");
+      this.tick();
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Witam {this.state.name}!</h1>
+        <h1>It's {this.state.date.toLocaleTimeString()}</h1>
+        <input
+          type="text"
+          onChange={(event) => {
+            console.log(event);
+            this.handleChange(event);
+          }}
+        />
+      </>
+    );
+  }
+}
+
+const app = (
+  <>
+    <StatefulComponent />
+  </>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
